c785af3044ebdd216402062b1bbda47084cc949c
vaibhavraj1806 <vaibhavrajofficials@gmail.com>
2022-12-22 19:57:07 +0530

    init method demo

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
     def calculate_total_price(self, x, y):
         return x*y
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 7
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Phone", 100, 5)
+
+item2 = Item("Laptop", 1000, 3)
+
+
+print(item1.name)
+print(item2.name)
+print(item1.price)
+print(item2.price)
+print(item1.quantity)
+print(item2.quantity)
