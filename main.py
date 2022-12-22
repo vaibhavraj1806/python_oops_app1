7daf6c48309d41696b5d5f4b7b6a7c3cec1fe8bd
vaibhavraj1806 <vaibhavrajofficials@gmail.com>
2022-12-22 20:14:31 +0530

    init method instance attribute demo

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+
+        #Assign values to the self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x*y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item("Phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 3)
-
 
 print(item1.name)
 print(item2.name)
@@ -19,3 +24,5 @@ print(item1.price)
 print(item2.price)
 print(item1.quantity)
 print(item2.quantity)
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
